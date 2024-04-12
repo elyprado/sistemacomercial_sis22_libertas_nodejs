ef9df9c668e36877a34e7424a18d162280a9b61c
Gabriel Paulino Alves <128816401+Gabriel-Paulino-Alves@users.noreply.github.com>
2024-04-12 10:02:14 -0300

    Update inserindo a const ProdutoRoute no index.js

--- src/index.js
+++ src/index.js
@@ -1,7 +1,9 @@
 //index.js
 const UsuarioRoute = require('./UsuarioRoute');
 const VendedorRoute = require('./VendedorRoute');
+const ProdutoRoute = require('./ProdutoRoute');
 module.exports = (app) => {
    UsuarioRoute(app)
    VendedorRoute(app)
+   ProdutoRoute(app)
 }
