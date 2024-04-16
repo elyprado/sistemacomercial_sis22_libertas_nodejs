fd84af7f19a05adcfc22230c0228210478aea99a
Felipe <felipe.passagem@gmail.com>
2024-04-16 20:30:27 -0300

    .

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@
 const UsuarioRoute = require('./UsuarioRoute');
 const VendedorRoute = require('./VendedorRoute');
 const ProdutoRoute = require('./ProdutoRoute');
+const VendaRoute = require('./VendaRoute');
 module.exports = (app) => {
    UsuarioRoute(app)
    VendedorRoute(app)
    ProdutoRoute(app)
+   VendaRoute(app)
 }
